d67bd3667b2bb8a84270791f398d6274e8bd8538
yangxin2 <yang657850144@qq.com>
2018-12-01 13:09:59 +0800

    feat: use eslint-formatter-friendly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,20 @@
 module.exports = {
+  "env": {
+    "es6": true,
+    "browser": true,
+    "node": true,
+  },
   extends: [
     'standard',
     'plugin:vue/essential'
   ],
   parserOptions:{
-    "parser": "babel-eslint"
+    "parser": "babel-eslint",
+    "ecmaVersion": 2017,
+    "sourceType": "module",
+    "ecmaFeatures ": {
+      "jsx": true,
+      "modules": true
+    }
   }
 }
